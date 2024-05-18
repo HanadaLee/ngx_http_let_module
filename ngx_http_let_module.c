db3a5bf0a3b5bb9cc2d6e128306a51d5211d4ce3
Hanada <im@hanada.info>
2024-05-19 02:15:45 +0800

    Add more function and let var nocacheble

--- ngx_http_let_module.c
+++ ngx_http_let_module.c
@@ -138,7 +138,7 @@ NGX_LET_HASHFUNC(SHA512, sha512, 64)
 
 NGX_LET_HASHFUNC(RIPEMD160, ripemd160, 20)
 
-static ngx_int_t ngx_let_func_length(ngx_http_request_t *r, 
+static ngx_int_t ngx_let_func_len(ngx_http_request_t *r, 
         ngx_str_t *str, ngx_str_t *ret)
 {
     ret->len = 32;
@@ -231,6 +231,389 @@ static ngx_int_t ngx_let_func_substr(ngx_http_request_t *r,
     return NGX_OK;
 }
 
+static ngx_int_t ngx_let_func_position(ngx_http_request_t *r,
+        ngx_str_t *s, ngx_str_t *s1, ngx_str_t *ret)
+{
+    u_char *p;
+    u_char *last;
+
+    ret->len = NGX_OFF_T_LEN;
+    ret->data = ngx_palloc(r->pool, ret->len);
+    if (ret->data == NULL) {
+        return NGX_ERROR;
+    }
+
+    if (s == NULL || s1 == NULL || s->len == 0 || s1->len == 0) {
+        ret->len = 0;
+        return NGX_OK;
+    }
+
+    last = s->data + s->len - s1->len;
+
+    for (p = s->data; p <= last; p++) {
+        if (ngx_strncmp(p, s1->data, s1->len) == 0) {
+            ngx_int_t pos = p - s->data;
+            ret->len = ngx_sprintf(ret->data, "%d", pos) - ret->data;
+            return NGX_OK;
+        }
+    }
+
+    ret->len = 0;
+    return NGX_OK;
+}
+
+static ngx_int_t ngx_let_func_trim(ngx_http_request_t *r,
+        ngx_str_t *str, ngx_str_t *ret)
+{
+    u_char *start, *end;
+    ngx_uint_t i;
+
+    start = str->data;
+    end = str->data + str->len - 1;
+
+    while (start <= end && (*start == ' ' || *start == '\t' || *start == '\n' || *start == '\r')) {
+        start++;
+    }
+
+    while (end >= start && (*end == ' ' || *end == '\t' || *end == '\n' || *end == '\r')) {
+        end--;
+    }
+
+    ret->len = end >= start ? (ngx_uint_t)(end - start + 1) : 0;
+    ret->data = ngx_palloc(r->pool, ret->len);
+    if (ret->data == NULL) {
+        return NGX_ERROR;
+    }
+
+    for (i = 0; i < ret->len; i++) {
+        ret->data[i] = start[i];
+    }
+
+    return NGX_OK;
+}
+
+static ngx_int_t ngx_let_func_ltrim(ngx_http_request_t *r,
+        ngx_str_t *str, ngx_str_t *ret)
+{
+    u_char *start;
+    ngx_uint_t i;
+
+    start = str->data;
+
+    while (start < str->data + str->len && (*start == ' ' || *start == '\t' || *start == '\n' || *start == '\r')) {
+        start++;
+    }
+
+    ret->len = str->len - (start - str->data);
+    ret->data = ngx_palloc(r->pool, ret->len);
+    if (ret->data == NULL) {
+        return NGX_ERROR;
+    }
+
+    for (i = 0; i < ret->len; i++) {
+        ret->data[i] = start[i];
+    }
+
+    return NGX_OK;
+}
+
+static ngx_int_t ngx_let_func_rtrim(ngx_http_request_t *r,
+        ngx_str_t *str, ngx_str_t *ret)
+{
+    u_char *end;
+    ngx_uint_t i;
+
+    end = str->data + str->len - 1;
+
+    while (end >= str->data && (*end == ' ' || *end == '\t' || *end == '\n' || *end == '\r')) {
+        end--;
+    }
+
+    ret->len = end >= str->data ? (ngx_uint_t)(end - str->data + 1) : 0;
+    ret->data = ngx_palloc(r->pool, ret->len);
+    if (ret->data == NULL) {
+        return NGX_ERROR;
+    }
+
+    for (i = 0; i < ret->len; i++) {
+        ret->data[i] = str->data[i];
+    }
+
+    return NGX_OK;
+}
+
+static ngx_int_t ngx_let_func_replace(ngx_http_request_t *r,
+        ngx_str_t *s, ngx_str_t *s1,
+        ngx_str_t *s2, ngx_str_t *ret)
+{
+    u_char *p, *last;
+    ngx_uint_t count = 0;
+    size_t new_len;
+    u_char *new_data, *dst;
+
+    if (s == NULL || s1 == NULL || s2 == NULL || s->len == 0 || s1->len == 0) {
+        ret->len = 0;
+        ret->data = NULL;
+        return NGX_OK;
+    }
+
+    last = s->data + s->len - s1->len;
+    for (p = s->data; p <= last; p++) {
+        if (ngx_strncmp(p, s1->data, s1->len) == 0) {
+            count++;
+            p += s1->len - 1;
+        }
+    }
+
+    if (count == 0) {
+        ret->len = s->len;
+        ret->data = ngx_palloc(r->pool, ret->len);
+        if (ret->data == NULL) {
+            return NGX_ERROR;
+        }
+        ngx_memcpy(ret->data, s->data, s->len);
+        return NGX_OK;
+    }
+
+    new_len = s->len + count * (s2->len - s1->len);
+
+    new_data = ngx_palloc(r->pool, new_len);
+    if (new_data == NULL) {
+        return NGX_ERROR;
+    }
+
+    dst = new_data;
+    p = s->data;
+    last = s->data + s->len;
+
+    while (p < last) {
+        if (ngx_strncmp(p, s1->data, s1->len) == 0) {
+            ngx_memcpy(dst, s2->data, s2->len);
+            dst += s2->len;
+            p += s1->len;
+        } else {
+            *dst++ = *p++;
+        }
+    }
+
+    ret->len = new_len;
+    ret->data = new_data;
+
+    return NGX_OK;
+}
+
+static ngx_int_t ngx_let_func_reverse(ngx_http_request_t *r,
+        ngx_str_t *str, ngx_str_t *ret)
+{
+    ngx_uint_t i;
+
+    ret->len = str->len;
+    ret->data = ngx_palloc(r->pool, ret->len);
+    if (ret->data == NULL) {
+        return NGX_ERROR;
+    }
+
+    for (i = 0; i < str->len; i++) {
+        ret->data[i] = str->data[str->len - 1 - i];
+    }
+
+    return NGX_OK;
+}
+
+static ngx_int_t ngx_let_func_rand_int(ngx_http_request_t *r,
+        ngx_str_t *start_str, ngx_str_t *end_str,
+        ngx_str_t *ret)
+{
+    ngx_int_t start, end, tmp, random;
+
+    start = ngx_atoi(start_str->data, start_str->len);
+    if (start == NGX_ERROR) {
+        ngx_log_error(NGX_LOG_NOTICE, r->connection->log, 0,
+                      "rand_int: bad \"start\" argument: %V", start_str);
+        ret->len = 0;
+        return NGX_OK;
+    }
+
+    end = ngx_atoi(end_str->data, end_str->len);
+    if (end == NGX_ERROR) {
+        ngx_log_error(NGX_LOG_NOTICE, r->connection->log, 0,
+                      "rand_int: bad \"end\" argument: %V", end_str);
+        ret->len = 0;
+        return NGX_OK;
+    }
+
+    if (start > end) {
+        tmp = start;
+        start = end;
+        end = tmp;
+    }
+
+    random = rand() % (end - start + 1) + start;
+
+    ret->data = ngx_palloc(r->pool, NGX_INT_T_LEN);
+    if (ret->data == NULL) {
+        return NGX_ERROR;
+    }
+
+    ret->len = ngx_sprintf(ret->data, "%i", random) - ret->data;
+
+    return NGX_OK;
+}
+
+static ngx_int_t ngx_let_func_repeat(ngx_http_request_t *r,
+        ngx_str_t *str, ngx_str_t *n_str,
+        ngx_str_t *ret)
+{
+    ngx_uint_t i, n;
+    size_t total_len;
+
+    n = ngx_atoi(n_str->data, n_str->len);
+    if (n == (ngx_uint_t) NGX_ERROR || n == 0) {
+        ngx_log_error(NGX_LOG_NOTICE, r->connection->log, 0,
+                      "repeat: bad \"n\" argument: %V", n_str);
+        ret->len = 0;
+        return NGX_OK;
+    }
+
+    total_len = str->len * n;
+
+    ret->data = ngx_palloc(r->pool, total_len);
+    if (ret->data == NULL) {
+        return NGX_ERROR;
+    }
+
+    for (i = 0; i < n; i++) {
+        ngx_memcpy(ret->data + i * str->len, str->data, str->len);
+    }
+
+    ret->len = total_len;
+
+    return NGX_OK;
+}
+
+static ngx_int_t ngx_let_is_valid_number(u_char *data, size_t len)
+{
+    size_t i;
+    int dot_count = 0;
+
+    for (i = 0; i < len; i++) {
+        if (data[i] == '.') {
+            dot_count++;
+            if (dot_count > 1) {
+                return NGX_ERROR;
+            }
+        } else if (data[i] < '0' || data[i] > '9') {
+            return NGX_ERROR;
+        }
+    }
+
+    return NGX_OK;
+}
+
+static ngx_int_t ngx_let_func_round(ngx_http_request_t *r,
+        ngx_str_t *num_str, ngx_str_t *digits_str,
+        ngx_str_t *ret)
+{
+    ngx_int_t digits, i, j, decimal_point = -1, len;
+    u_char *num_data, *result;
+    size_t num_len;
+
+    digits = ngx_atoi(digits_str->data, digits_str->len);
+    if (digits == NGX_ERROR || digits < 0) {
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0,
+                      "round: bad \"digits\" argument: %V", digits_str);
+        ret->len = 0;
+        return NGX_OK;
+    }
+
+    if (ngx_let_is_valid_number(num_str->data, num_str->len) != NGX_OK) {
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0,
+                      "round: bad \"num\" argument: %V", num_str);
+        ret->len = 0;
+        return NGX_OK;
+    }
+
+    num_data = num_str->data;
+    num_len = num_str->len;
+
+    for (i = 0; i < (ngx_int_t)num_len; i++) {
+        if (num_data[i] == '.') {
+            decimal_point = i;
+            break;
+        }
+    }
+
+    if (decimal_point == -1) {
+        decimal_point = num_len;
+        num_len += digits + 1;
+        num_data = ngx_palloc(r->pool, num_len + 1);
+        if (num_data == NULL) {
+            return NGX_ERROR;
+        }
+        ngx_memcpy(num_data, num_str->data, num_str->len);
+        num_data[decimal_point] = '.';
+        for (i = decimal_point + 1; i < (ngx_int_t)num_len; i++) {
+            num_data[i] = '0';
+        }
+        num_data[num_len] = '\0';
+        num_len = num_len;
+    }
+
+    len = decimal_point + digits + 1;
+    if (len > (ngx_int_t)num_len) {
+        len = num_len;
+    }
+
+    result = ngx_palloc(r->pool, len + 1);
+    if (result == NULL) {
+        return NGX_ERROR;
+    }
+
+    ngx_memcpy(result, num_data, len);
+    result[len] = '\0';
+
+    if (len < (ngx_int_t)num_len && num_data[len] >= '5') {
+        for (j = len - 1; j >= 0; j--) {
+            if (result[j] == '.') {
+                continue;
+            }
+
+            if (result[j] < '9') {
+                result[j]++;
+                break;
+            } else {
+                result[j] = '0';
+                if (j == 0) {
+                    u_char *new_result = ngx_palloc(r->pool, len + 2);
+                    if (new_result == NULL) {
+                        return NGX_ERROR;
+                    }
+                    new_result[0] = '1';
+                    ngx_memcpy(new_result + 1, result, len);
+                    new_result[len + 1] = '\0';
+                    ret->data = new_result;
+                    ret->len = len + 1;
+                    return NGX_OK;
+                }
+            }
+        }
+    }
+
+    if (len < (decimal_point + digits + 1)) {
+        for (i = len; i < (decimal_point + digits + 1); i++) {
+            result[i] = '0';
+        }
+        result[decimal_point + digits + 1] = '\0';
+        ret->len = decimal_point + digits + 1;
+    } else {
+        ret->len = len;
+    }
+
+    ret->data = result;
+
+    return NGX_OK;
+}
+
 /* Call function by name & return result */
 static ngx_int_t ngx_let_call_fun(ngx_http_request_t *r,
         ngx_str_t *name, ngx_array_t *args, ngx_str_t *value)
@@ -284,14 +667,22 @@ static ngx_int_t ngx_let_call_fun(ngx_http_request_t *r,
     CALL_FUNC_1(ripemd160);
 
     /* string operations */
-    CALL_FUNC_1(length);
+    CALL_FUNC_1(len);
     CALL_FUNC_1(lower);
     CALL_FUNC_1(upper);
+    CALL_FUNC_1(trim);
+    CALL_FUNC_1(ltrim);
+    CALL_FUNC_1(rtrim);
+    CALL_FUNC_1(reverse);
+    CALL_FUNC_2(position);
     CALL_FUNC_3(substr);
+    CALL_FUNC_3(replace);
 
     /* integer operations */
+    CALL_FUNC_2(rand_int);
     CALL_FUNC_2(max);
     CALL_FUNC_2(min);
+    CALL_FUNC_2(round);
 
     ngx_log_error(NGX_LOG_ALERT, r->connection->log, 0,
                 "let undefined function '%*s'", name->len, name->data);
@@ -567,7 +958,7 @@ static char* ngx_http_let_let(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
     value[1].data++;
     value[1].len--;
 
-    v = ngx_http_add_variable(cf, &value[1], NGX_HTTP_VAR_CHANGEABLE);
+    v = ngx_http_add_variable(cf, &value[1], NGX_HTTP_VAR_CHANGEABLE|NGX_HTTP_VAR_NOCACHEABLE);
 
     v->get_handler = ngx_http_let_variable;
     v->data = (uintptr_t)ngx_parse_let_expr(cf);
